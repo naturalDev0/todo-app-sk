40a9dadb1251e18458832cfdd9533bdaf09908d5
Tommy Lee <tonglin.lee@gmail.com>
2020-08-22 20:21:35 +0800

    Imported packages 'pymongo' to perform CRUD operations using MongoDB Atlas and 'decouple' to read environment variables from app.py Added url_for to route back to homepage, Changed the placeholder and id of input field, and Added 'block' jinja2 component for extension from base.html Added data retrieved from app.py and have it populated to individual cards in index.html, both the 'text' and 'complete' fields.

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
-# from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
+from decouple import config
 
 app = Flask(__name__)
 
+# User credentials
+usr = config('MONGO_DB_USER')
+pwd = config('MONGO_DB_PASS')
+db_collection = config('MONGO_COLLECTION')
+
+# 
+client  = pymongo.MongoClient("mongodb+srv://" + usr + ":" + pwd + "@clusterx.pg3wu.mongodb.net/"+ db_collection +"?retryWrites=true&w=majority")
+
+db = client['todo']
+collection = db['tasks']
+# collection = pymongo.collection.Collection(db, 'tasks')
+
+
 @app.route('/')
 def index():
-    return render_template("base.html")
+    cursor = collection.find({})
+    return render_template("index.html", data=cursor)
+
+@app.route('/test')
+def test():
+    collection.insert({
+        "text" : "hihi",
+        "complete" : False
+    })
+    return '<h1>Data has been inserted!</h1>'
+
+# @app.route('/verify')
+# def verify():
+#     return '<h1>' + pwd + '</h1>'
